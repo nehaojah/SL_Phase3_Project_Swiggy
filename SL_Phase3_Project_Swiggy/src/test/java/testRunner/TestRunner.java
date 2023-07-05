0ca17fbfa5fd1cd3f8004040416f995831b5b173
nehaojah <neha.fsm@gmail.com>
2023-07-05 17:37:17 +0200

    fresh commit

--- SL_Phase3_Project_Swiggy/src/test/java/testRunner/TestRunner.java
+++ SL_Phase3_Project_Swiggy/src/test/java/testRunner/TestRunner.java
@@ -1,5 +1,9 @@
 package testRunner;
 
-public class TestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features="src/test/java/features/SearchAndAddToCart.feature", glue="stepDefinitions", plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
